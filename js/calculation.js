c4a1316d47d2a6910e4ca6a25a053a523e00a105
MD RIGAN <moshiur251@gmail.com>
2022-02-19 21:44:12 +0600

    final-commit

--- js/calculation.js
+++ js/calculation.js
@@ -1,29 +1,36 @@
-// collect input values
+//custom function for collect input values
 function getInput(id){
     const inputFieldValue = document.getElementById(id + '-input');
     const inputValue = parseInt(inputFieldValue.value);
     return inputValue
 
 }
+//custom function for collect html element
 function getElement(id){
     const theElement = document.getElementById(id);
     return theElement
 
 }
+//custom function for calculate percentage
 function getPercentage(percent, total) {
     return parseInt(((percent/ 100) * total).toFixed(2))
 }
 
 //click button Calculation to do something
-document.getElementById('calculate-button').addEventListener('click',function(){
+document.getElementById('calculate-button').addEventListener('click',function(){   
+    //hide previous erros messages after clicking calculate again
+    getElement('input-error').style.display = 'none'
+    getElement('cost-error').style.display = 'none'
+    getElement('negetive-error').style.display = 'none'
+
     // total income 
     const incomeAmount = getInput('income')
     //mothly expenditures
     const foodExpennseAmount = getInput('food')
     const rentExpeseAmount = getInput('rent')
     const clothExpeseAmount = getInput('cloth')
-
-    if(isNaN(incomeAmount)==true || isNaN(rentExpeseAmount)==true || isNaN(foodExpennseAmount)==true || isNaN(clothExpeseAmount)==true){
+    //set condition for NaN or negitive inputs
+    if(isNaN(incomeAmount)==true||isNaN(rentExpeseAmount)==true||isNaN(foodExpennseAmount)==true||isNaN(clothExpeseAmount)==true|| incomeAmount<0||rentExpeseAmount<0||foodExpennseAmount<0|| clothExpeseAmount<0){
         getElement('input-error').style.display = 'block'
     }else{
         //total expense
@@ -46,21 +53,33 @@ document.getElementById('calculate-button').addEventListener('click',function(){
 
 //click save button
 document.getElementById('save-button').addEventListener('click', function(){
+    //hide previous erros messages after clicking calculate again
+    getElement('savings-error').style.display = 'none'
+    getElement('input-error').style.display = 'none'
     // total income 
     const incomeAmount = getInput('income')
     //mothly expenditures
     const foodExpennseAmount = getInput('food')
     const rentExpeseAmount = getInput('rent')
     const clothExpeseAmount = getInput('cloth')
-
+    //total expenses
     const totalExpense = foodExpennseAmount + rentExpeseAmount + clothExpeseAmount;
     const remainnigBalance = incomeAmount - totalExpense
-
+    //setting percentage calculations
     const percent = getInput('percentage');
-
     const percentage = getPercentage(percent , incomeAmount);
-
-    getElement('saving-amount').innerText = percentage;
-
-
+    //after all remainning balance
+    const totalRemainningBalance = remainnigBalance - percentage
+    if(isNaN(percent)==true ||percent<0){
+        getElement('input-error').style.display = 'block'
+    }else{
+        if(percentage > remainnigBalance){
+            getElement('savings-error').style.display = 'block'
+        }else{
+            getElement('saving-amount').innerText = percentage;
+            getElement('remainning-balance').innerText = totalRemainningBalance ;
+        }
+        
+    }
+    
 })
